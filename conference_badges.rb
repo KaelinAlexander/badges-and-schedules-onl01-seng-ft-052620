6ed83a2b95b16bb57f826f228810a17c356d12fa
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-28 17:00:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def batch_badge_creator(names)
   names.each do|name|
-  puts "Hello, #{name}! You'll be in room #{number[]+1}."
+  puts "Hello, #{name}! You'll be in room #{name[]}."
   end 
 end
 
